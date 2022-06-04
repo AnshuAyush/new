858bda51978d7aa0dab6d535db74df3073d4bca2
Anshu <anshuayush2002@gmail.com>
2022-06-04 18:54:10 +0530

    started server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 const express = require("express");
+const app = express();
+const PORT = 80;
+
+app.get("/", (req, res) =>{
+    res.send("Hello")
+})
+
+
+app.listen(PORT, (req, res)=>{
+    console.log(`The app is running on ${PORT} port`);  
+})
